e9ae032aa916c68a4a3877a0c585a93617e95c34
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-06-09 21:19:34 +0700

    fixed4

--- resources/views/management-item.php
+++ resources/views/management-item.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="public/css/style.css" rel="stylesheet">
     <link href="public/css/index.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="public/img/icon.png">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 </head>
 
@@ -29,7 +30,7 @@
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <h1>Quản lí kho hàng</h1>
                     </div>
-                    
+
                     <div class="row mb-4">
                         <div class="d-flex justify-content-between align-items-center">
                             <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#trash-can-modal'>
--- resources/views/management-order.php
+++ resources/views/management-order.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="public/css/style.css" rel="stylesheet">
     <link href="public/css/index.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="public/img/icon.png">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 </head>
 
